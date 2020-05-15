3431660f45930211f6c0fac1f1ad2cdb03626909
Jakob Lange <webjakob@gmail.com>
2020-05-15 18:25:41 +0200

    Favicons + PWA

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+// Inside vue.config.js
+module.exports = {
+  // ...other vue-cli plugin options...
+  pwa: {
+    name: 'Color A11y',
+    themeColor: '#000',
+    msTileColor: '#fff',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'GenerateSW'
+  }
+};
